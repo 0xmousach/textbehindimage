7296f31b5ff5da3cf13ba281d5448136d20923b6
Mousa Cheema <wxe7zk@virginia.edu>
2025-09-03 01:47:27 -0400

    Project setup

---
+++ Backend/routes/designRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import {
+    // Controllers to be imported
+} from "../controllers/designController.js";
+
+const router = express.Router();
+
+// Routes to be defined here
+
+export default router;
--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,10 @@
 import express from "express"
+import designRoutes from "./routes/designRoutes.js"
 
-const app = express()
+const app = express();
+
+app.use("api/designs", designRoutes);
 
 app.listen(5001, () => {
     console.log("Server is running on port 5001")
 });
-
-app.use()
