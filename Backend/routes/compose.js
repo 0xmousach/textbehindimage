8c09940a9dc6f43a515079cdc0c16aa73e32e8e8
Abrar-135 <abrar.sajed.03@gmail.com>
2025-09-06 04:14:15 -0400

    added frontend

--- Backend/routes/compose.js
+++ Backend/routes/compose.js
@@ -2,11 +2,8 @@
 // e.g.,  POST /api/compose Do the whole process in one shot       
 
 import express from "express"
-<<<<<<< HEAD:Backend/src/routes/compose.js
-import { createImage } from "../controller/composeController.js"
-=======
 import { composeImage } from "../controller/composeController.js"
->>>>>>> ef4808e6caa5fbbcdcffb542fcefe84c588d2ab3:Backend/routes/compose.js
+
 
 const router = express.Router();
 
